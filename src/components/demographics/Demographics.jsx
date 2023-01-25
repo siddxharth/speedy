99f05c848de766548922a31420952ae5be707a5b
Siddharth Khatri <siddxharth@gmail.com>
2023-01-25 12:26:43 +0530

    Add Demographics section

--- src/components/demographics/Demographics.jsx
+++ src/components/demographics/Demographics.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import user from './user.svg';
+import location from './location.svg';
+import server from './server.svg';
+import './Demographics.css';
 
 export default function Demographics() {
 	return (
-		<div>Demographics</div>
+		<div className='demographic-section'>
+			<div className='demographic-content'>
+				<img src={user} alt='User' />
+				<div className='demographic-text'>
+					<div className='demographic-title'>900+</div>
+					<div className='demographic-sub'>Users</div>
+				</div>
+			</div>
+			<div className='demographic-content'>
+				<img src={location} alt='Location' />
+				<div className='demographic-text'>
+					<div className='demographic-title'>30+</div>
+					<div className='demographic-sub'>Locations</div>
+				</div>
+			</div>
+			<div className='demographic-content'>
+				<img src={server} alt='Server' />
+				<div className='demographic-text'>
+					<div className='demographic-title'>50+</div>
+					<div className='demographic-sub'>Servers</div>
+				</div>
+			</div>
+		</div>
 	)
 }
